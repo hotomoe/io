8ca6633ad23f82f16fc21aa61906d5db48744b60
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2024-09-01 08:57:30 -0700

    Fix time offsets for digital clock widget (#44)

--- packages/frontend/src/scripts/timezones.ts
+++ packages/frontend/src/scripts/timezones.ts
@@ -3,52 +3,16 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export const timezones = [{
-	name: 'UTC',
-	abbrev: 'UTC',
-	offset: 0,
-}, {
-	name: 'Europe/Berlin',
-	abbrev: 'CET',
-	offset: 60,
-}, {
-	name: 'Asia/Tokyo',
-	abbrev: 'JST',
-	offset: 540,
-}, {
-	name: 'Asia/Seoul',
-	abbrev: 'KST',
-	offset: 540,
-}, {
-	name: 'Asia/Shanghai',
-	abbrev: 'CST',
-	offset: 480,
-}, {
-	name: 'Australia/Sydney',
-	abbrev: 'AEST',
-	offset: 600,
-}, {
-	name: 'Australia/Darwin',
-	abbrev: 'ACST',
-	offset: 570,
-}, {
-	name: 'Australia/Perth',
-	abbrev: 'AWST',
-	offset: 480,
-}, {
-	name: 'America/New_York',
-	abbrev: 'EST',
-	offset: -300,
-}, {
-	name: 'America/Mexico_City',
-	abbrev: 'CST',
-	offset: -360,
-}, {
-	name: 'America/Phoenix',
-	abbrev: 'MST',
-	offset: -420,
-}, {
-	name: 'America/Los_Angeles',
-	abbrev: 'PST',
-	offset: -480,
-}];
+import moment from 'moment-timezone';
+
+export const timezones =
+[
+	'UTC', 'Europe/Berlin', 'Asia/Tokyo', 'Asia/Seoul', 'Asia/Shanghai',
+	'Australia/Sydney', 'Australia/Darwin', 'Australia/Perth',
+	'America/New_York', 'America/Mexico_City', 'America/Phoenix',
+	'America/Los_Angeles',
+].map((x) => ({
+	name: x,
+	abbrev: moment.tz.zone(x).abbr(Date.now()),
+	offset: -moment.tz.zone(x).utcOffset(Date.now()),
+}));
